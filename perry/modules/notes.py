0aa155bdb6731729e3db7dc489857c7d453c4b3f
marchingon12 <augustthegreat.cool@gmail.com>
2020-10-22 18:00:09 +0200

    Notes: Show link previews by default

--- perry/modules/notes.py
+++ perry/modules/notes.py
@@ -179,7 +179,7 @@ def get(bot, update, notename, show_none=True, no_format=False):
                         text,
                         reply_to_message_id=reply_id,
                         parse_mode=parseMode,
-                        disable_web_page_preview=True,
+                        disable_web_page_preview=False,
                         reply_markup=keyboard,
                     )
                 else:
@@ -197,7 +197,7 @@ def get(bot, update, notename, show_none=True, no_format=False):
                             caption=text,
                             reply_to_message_id=reply_id,
                             parse_mode=parseMode,
-                            disable_web_page_preview=True,
+                            disable_web_page_preview=False,
                             reply_markup=keyboard,
                         )
 
